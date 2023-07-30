86a1531388d45ac958a6f52bf8a2c37067e9e5bd
Ahmer <ahmerhabibb@gmail.com>
2023-07-30 17:58:34 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,20 +1,36 @@
 import test from 'tape';
 import getWebGLContext from '../src';
 
-test('should return the Web Graphic Library - context', t => {
+test('getWebGLContext - Should return the WebGL context for a canvas', t => {
   t.plan(1);
 
   const canvas = document.createElement('canvas');
   const gl = getWebGLContext(canvas);
 
-  t.ok(gl instanceof WebGLRenderingContext, 'Web Graphic Library context returned');
+  t.ok(gl instanceof WebGLRenderingContext, 'WebGL context returned');
 });
 
-test('Return null if !=', t => {
+test('getWebGLContext - Should return null for non-canvas elements', t => {
   t.plan(1);
 
   const div = document.createElement('div');
   const gl = getWebGLContext(div);
 
-  t.ok(gl === null, 'null returned');
+  t.equal(gl, null, 'null returned');
+});
+
+test('getWebGLContext - Should return null for null input', t => {
+  t.plan(1);
+
+  const gl = getWebGLContext(null);
+
+  t.equal(gl, null, 'null returned');
+});
+
+test('getWebGLContext - Should return null for undefined input', t => {
+  t.plan(1);
+
+  const gl = getWebGLContext(undefined);
+
+  t.equal(gl, null, 'null returned');
 });
